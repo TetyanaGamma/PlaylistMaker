64f6e6b5f8c8eecda49c7bff6feefaf3b6c5f95f
Гамма Татьяна <tetyanagamma@gmail.com>
2025-03-19 14:58:50 +0200

    Доделала задачу

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.playlistmaker
 
+import android.content.Intent
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,9 +14,42 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
+        val searchButton = findViewById<Button>(R.id.search_button)
+        //обрабока нажатия на кнопку поиск через лямбду
+        searchButton.setOnClickListener {
+            val searchIntent = Intent(this, SearchActivity::class.java)
+            startActivity(searchIntent)
+        }
+        /*обрабока нажатия на кнопку поиск через анонимный класс
+        val searchButtonClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                 val searchIntent = Intent(this, SearchActivity::class.java)
+                startActivity(searchIntent)
+            }
+        }
+        searchButton.setOnClickListener(searchButtonClickListener) */
 
+        val mediatekaButton = findViewById<Button>(R.id.library_button)
+        // обрабока нажатия на кнопку медиатека через лямбду
+        mediatekaButton.setOnClickListener {
+            val mediatekaIntent = Intent(this, MediatekaActivity::class.java)
+            startActivity(mediatekaIntent)
+        }
+        /* обрабока нажатия на кнопку медиатека через анонимный класс
+        val madiatekaButtonClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+             val mediatekaIntent = Intent(this, MediatekaActivity::class.java)
+            startActivity(mediatekaIntent)
+            }
+        }
+        mediatekaButton.setOnClickListener(madiatekaButtonClickListener) */
+
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+        settingsButton.setOnClickListener {
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
     }
 }

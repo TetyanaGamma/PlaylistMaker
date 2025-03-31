4dc24ff27fb18f31556d611116fdf00f698fe1a5
Гамма Татьяна <tetyanagamma@gmail.com>
2025-03-31 15:54:24 +0300

    1) реализован экран настройки 2) сверстан экран поиск

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,9 @@
 package com.example.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,5 +19,46 @@ class SettingsActivity : AppCompatActivity() {
         toolbar.setNavigationOnClickListener {
             finish()
         }
+        val shareMaterialTextView: TextView = findViewById(R.id.settings_share)
+        shareMaterialTextView.setOnClickListener {
+            shareText()
+        }
+        val supportMaterialTextView: TextView = findViewById(R.id.settings_support)
+        supportMaterialTextView.setOnClickListener {
+            messageToSupport()
+        }
+       val agreementMaterialTextView: TextView = findViewById(R.id.settings_user_agreement)
+        agreementMaterialTextView.setOnClickListener{
+            goToUserAgreement()
+        }
     }
+
+    fun shareText() {
+        val messageToShare = getText(R.string.massege_to_share)
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.putExtra(Intent.EXTRA_TEXT, messageToShare)
+        shareIntent.type = "text/plain"
+        val sendIntent = Intent.createChooser(shareIntent, null)
+        startActivity(sendIntent)
+    }
+
+    fun messageToSupport() {
+        val email = getText(R.string.my_email)
+        val emailSubject = getText(R.string.email_subject)
+        val emailBody = getText(R.string.email_body)
+
+        val supportIntent = Intent(Intent.ACTION_SEND)
+        supportIntent.data = Uri.parse("mailto:$email")
+        supportIntent.putExtra(Intent.EXTRA_SUBJECT, emailSubject)
+        supportIntent.putExtra(Intent.EXTRA_TEXT, emailBody)
+        startActivity(supportIntent)
+    }
+
+    fun goToUserAgreement() {
+        val agreementUrl = getText(R.string.user_agreement_url)
+        val agreementIntent = Intent(Intent.ACTION_VIEW)
+        agreementIntent.data = Uri.parse("$agreementUrl")
+        startActivity(agreementIntent)
+    }
+
 }
